58e277c442038a8152d9cced792f0e0db03b05b1
vapretito <titomanbarreto@gmail.com>
2025-04-22 13:52:09 -0300

    fondo cel

--- app.py
+++ app.py
@@ -63,6 +63,9 @@ def respuesta():
 
     return send_file(io.BytesIO(tts_response.content), mimetype="audio/mpeg")
 
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5000)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
